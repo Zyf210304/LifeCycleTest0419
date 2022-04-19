f52749fde625ab2f6402715275be43618a8b75b0
张亚飞 <672233580@qq.com>
2022-04-19 14:18:55 +0800

    Initial Commit

---
+++ LifeCycleTest0419/LifeCycleTest0419App.swift
@@ -0,0 +1,17 @@
+//
+//  LifeCycleTest0419App.swift
+//  LifeCycleTest0419
+//
+//  Created by 张亚飞 on 2022/4/19.
+//
+
+import SwiftUI
+
+@main
+struct LifeCycleTest0419App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
